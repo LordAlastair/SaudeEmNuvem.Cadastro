331755f136602b5c50c2210cac590105aeefae73
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2018-04-08 21:10:53 -0300

    Tornando CreateDbContext um método estático;

--- SaudeEmNuvem.Cadastro.Infrastructure/CadastroContext.cs
+++ SaudeEmNuvem.Cadastro.Infrastructure/CadastroContext.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using SaudeEmNuvem.Cadastro.Domain.AggregatesModel.PacienteAggregate;
 using SaudeEmNuvem.Cadastro.Domain.SeedWork;
 using SaudeEmNuvem.Cadastro.Infrastructure.EntityConfigurations;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SaudeEmNuvem.Cadastro.Infrastructure
 {
-    public class CadastroContext
+    public sealed class CadastroContext
         : DbContext, IUnitOfWork
     {
         public const string DEFAULT_SCHEMA = "cadastro";
@@ -22,13 +22,11 @@ namespace SaudeEmNuvem.Cadastro.Infrastructure
 
         private readonly IMediator _mediator;
 
-        private CadastroContext(DbContextOptions<CadastroContext> options) : base(options) { }
-
         private CadastroContext(DbContextOptions<CadastroContext> options, IMediator mediator) : base(options)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
 
-            System.Diagnostics.Debug.WriteLine("CadastroContext::ctor ->" + this.GetHashCode());
+            System.Diagnostics.Debug.WriteLine("CadastroContext::ctor ->" + GetHashCode());
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -42,18 +40,18 @@ namespace SaudeEmNuvem.Cadastro.Infrastructure
             modelBuilder.ApplyConfiguration(new TipoTelefoneEntityTypeConfiguration());
         }
 
-        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken)
         {
             await _mediator.DispatchDomainEventsAsync(this);
 
-            var result = await base.SaveChangesAsync();
+            await SaveChangesAsync(cancellationToken);
 
             return true;
         }
 
         public class CadastroContextDesignFactory : IDesignTimeDbContextFactory<CadastroContext>
         {
-            public CadastroContext CreateDbContext(string[] args)
+            public static CadastroContext CreateDbContext(string[] args)
             {
                 var optionsBuilder = new DbContextOptionsBuilder<CadastroContext>()
                     .UseSqlServer("Server=.;Initial Catalog=SaudeEmNuvem.Services.Cadastro;Integrated Security=true");
@@ -63,20 +61,25 @@ namespace SaudeEmNuvem.Cadastro.Infrastructure
 
             class NoMediator : IMediator
             {
-                public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken)) where TNotification : INotification
+                public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken) where TNotification : INotification
                 {
                     return Task.CompletedTask;
                 }
 
-                public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
+                public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken)
                 {
-                    return Task.FromResult<TResponse>(default(TResponse));
+                    return Task.FromResult(default(TResponse));
                 }
 
-                public Task Send(IRequest request, CancellationToken cancellationToken = default(CancellationToken))
+                public Task Send(IRequest request, CancellationToken cancellationToken)
                 {
                     return Task.CompletedTask;
                 }
+            }
+
+            CadastroContext IDesignTimeDbContextFactory<CadastroContext>.CreateDbContext(string[] args)
+            {
+                return CreateDbContext(args);
             }
         }
     }

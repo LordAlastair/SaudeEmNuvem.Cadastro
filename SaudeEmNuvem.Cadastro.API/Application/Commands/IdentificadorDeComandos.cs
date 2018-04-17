e8e9ea7be43146efeb2532c4f163d2274738cbc0
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2018-04-17 19:35:51 -0300

    Comandos basicos para resolver a Idempotent nos comandos.

---
+++ SaudeEmNuvem.Cadastro.API/Application/Commands/IdentificadorDeComandos.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+
+namespace SaudeEmNuvem.Cadastro.API.Application.Commands
+{
+    public class IdentificadorDeComandos<T, TR> : IRequest<TR> where T : IRequest<TR>
+    {
+        public T Command { get; }
+        public Guid Id { get; }
+        public IdentificadorDeComandos(T command, Guid id)
+        {
+            Command = command;
+            Id = id;
+        }
+    }
+}

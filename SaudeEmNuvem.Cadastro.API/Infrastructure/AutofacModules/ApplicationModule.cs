d7f355ead06cb5da8596dbf653406cd365da4a37
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2018-04-24 20:50:36 -0300

    Configurando o projeto web.

--- SaudeEmNuvem.Cadastro.API/Infrastructure/AutofacModules/ApplicationModule.cs
+++ SaudeEmNuvem.Cadastro.API/Infrastructure/AutofacModules/ApplicationModule.cs
@@ -1,35 +1,21 @@
 using Autofac;
-using SaudeEmNuvem.Cadastro.BuildingBlocks.EventBus.Abstractions;
+using SaudeEmNuvem.BuildingBlocks.EventBus.Abstractions;
 using SaudeEmNuvem.Cadastro.API.Application.Commands;
-using SaudeEmNuvem.Cadastro.API.Application.Queries;
-using SaudeEmNuvem.Cadastro.Domain.AggregatesModel.Paciente;
 using SaudeEmNuvem.Cadastro.Infrastructure.Idempotency;
-using SaudeEmNuvem.Cadastro.Infrastructure.Repositories;
 using System.Reflection;
 
 namespace SaudeEmNuvem.Cadastro.API.Infrastructure.AutofacModules
 {
     public class ApplicationModule
-        :Autofac.Module
+        : Autofac.Module
     {
-        public string QueriesConnectionString { get; }
-
-        public ApplicationModule(string qconstr)
-        {
-            QueriesConnectionString = qconstr;
-        }
-
         protected override void Load(ContainerBuilder builder)
         {
-            builder.Register(c => new PacienteQueries(QueriesConnectionString))
-                .As<IPacienteQueries>()
-                .InstancePerLifetimeScope();
-
             builder.RegisterType<RequestManager>()
                .As<IRequestManager>()
                .InstancePerLifetimeScope();
 
-            builder.RegisterAssemblyTypes(typeof(CriarPacienteCommandHandler).GetTypeInfo().Assembly)
+            builder.RegisterAssemblyTypes(typeof(CriarCadastroCommand).GetTypeInfo().Assembly)
                 .AsClosedTypesOf(typeof(IIntegrationEventHandler<>));
 
         }
--- SaudeEmNuvem.Cadastro.API/Startup.cs
+++ SaudeEmNuvem.Cadastro.API/Startup.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using SaudeEmNuvem.BuildingBlocks.EventBus;
+using SaudeEmNuvem.BuildingBlocks.EventBus.Abstractions;
+using SaudeEmNuvem.BuildingBlocks.EventBus.RabbitMQ;
+using SaudeEmNuvem.BuildingBlocks.EventBus.Service;
+using SaudeEmNuvem.BuildingBlocks.HealthChecks.Extensions;
+using SaudeEmNuvem.BuildingBlocks.HealthChecks.SqlServer;
+using System;
 
 namespace SaudeEmNuvem.Cadastro.API
 {
@@ -21,20 +27,129 @@ namespace SaudeEmNuvem.Cadastro.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //add health check for this service
+            services.AddHealthChecks(checks =>
+            {
+                var minutes = 1;
+
+                if (int.TryParse(Configuration["HealthCheck:Timeout"], out var minutesParsed))
+                {
+                    minutes = minutesParsed;
+                }
+                checks.AddSqlCheck("OrderingDb", Configuration["ConnectionString"], TimeSpan.FromMinutes(minutes));
+            });
+
+            //configure settings
+
+            services.AddOptions();
+
+            //configure event bus related services
+
+            if (Configuration.GetValue<bool>("AzureServiceBusEnabled"))
+            {
+                services.AddSingleton<IServiceBusPersisterConnection>(sp =>
+                {
+                    var logger = sp.GetRequiredService<ILogger<DefaultServiceBusPersisterConnection>>();
+
+                    var serviceBusConnectionString = Configuration["EventBusConnection"];
+                    var serviceBusConnection = new ServiceBusConnectionStringBuilder(serviceBusConnectionString);
+
+                    return new DefaultServiceBusPersisterConnection(serviceBusConnection, logger);
+                });
+            }
+            else
+            {
+                services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+                {
+                    var logger = sp.GetRequiredService<ILogger<DefaultRabbitMqPersistentConnection>>();
+
+
+                    var factory = new ConnectionFactory()
+                    {
+                        HostName = Configuration["EventBusConnection"]
+                    };
+
+                    if (!string.IsNullOrEmpty(Configuration["EventBusUserName"]))
+                    {
+                        factory.UserName = Configuration["EventBusUserName"];
+                    }
+
+                    if (!string.IsNullOrEmpty(Configuration["EventBusPassword"]))
+                    {
+                        factory.Password = Configuration["EventBusPassword"];
+                    }
+
+                    var retryCount = 5;
+                    if (!string.IsNullOrEmpty(Configuration["EventBusRetryCount"]))
+                    {
+                        retryCount = int.Parse(Configuration["EventBusRetryCount"]);
+                    }
+
+                    return new DefaultRabbitMqPersistentConnection(factory, logger, retryCount);
+                });
+            }
+
+            RegisterEventBus(services);
+
+            //create autofac based service provider
+            var container = new ContainerBuilder();
+            container.Populate(services);
+
+
+            return new AutofacServiceProvider(container.Build());
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+            app.Map("/liveness", lapp => lapp.Run(async ctx => ctx.Response.StatusCode = 200));
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        }
+
+
+        private void RegisterEventBus(IServiceCollection services)
+        {
+            var subscriptionClientName = Configuration["SubscriptionClientName"];
+
+            if (Configuration.GetValue<bool>("AzureServiceBusEnabled"))
             {
-                app.UseDeveloperExceptionPage();
+                services.AddSingleton<IEventBus, EventBusServiceBus>(sp =>
+                {
+                    var serviceBusPersisterConnection = sp.GetRequiredService<IServiceBusPersisterConnection>();
+                    var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                    var logger = sp.GetRequiredService<ILogger<EventBusServiceBus>>();
+                    var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+
+                    return new EventBusServiceBus(serviceBusPersisterConnection, logger,
+                        eventBusSubcriptionsManager, subscriptionClientName, iLifetimeScope);
+                });
+            }
+            else
+            {
+                services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
+                {
+                    var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                    var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                    var logger = sp.GetRequiredService<ILogger<EventBusRabbitMQ>>();
+                    var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+
+                    var retryCount = 5;
+                    if (!string.IsNullOrEmpty(Configuration["EventBusRetryCount"]))
+                    {
+                        retryCount = int.Parse(Configuration["EventBusRetryCount"]);
+                    }
+
+                    return new EventBusRabbitMQ(rabbitMQPersistentConnection, logger, iLifetimeScope,
+                        eventBusSubcriptionsManager, subscriptionClientName, retryCount);
+                });
             }
 
-            app.UseMvc();
+            services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
         }
     }
 }
